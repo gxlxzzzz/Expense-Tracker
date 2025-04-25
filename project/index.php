4a8fb3af8813268918ad9da5ac0127b66e56e4b7
WhiteKnees <121643615+gxlxzzzz@users.noreply.github.com>
2025-04-25 22:32:01 +0100

    feat: index.php now created and styled
    style: style.css created and started.

--- project/index.php
+++ project/index.php
@@ -1,5 +1,30 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Expense Tracker</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<header>
+    <h1>Expense Tracker</h1>
+    <div class="auth-buttons">
+        <a class="btn" href="login.php">Login</a>
+        <a class="btn" href="register.php">Register</a>
+    </div>
+</header>
 
-include "database/db_connect.php";
+<main>
+    <div class="content">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque lacinia magna vitae justo tristique vulputate. Integer in laoreet justo. Pellentesque efficitur lacus vitae arcu elementum consequat. Curabitur ac maximus leo. Curabitur ac sollicitudin sem. Integer posuere magna a purus sollicitudin rhoncus. Maecenas pretium orci elit, vel placerat turpis condimentum at. Proin vitae erat leo. Sed et eros eget velit congue tincidunt vitae ac augue.</p>
+        <p>Curabitur quis feugiat arcu. Proin a ullamcorper sapien. Cras a eros eget dui aliquam scelerisque. Pellentesque vel odio dolor. Donec sed magna porta, eleifend nisl vel, interdum nibh. Duis consectetur ante ut erat efficitur tincidunt. Ut eget maximus felis, a elementum purus. Nam arcu tortor, vestibulum sit amet ornare id, posuere quis augue. Quisque id tellus accumsan, lobortis lectus dapibus, lacinia nunc. Vestibulum vel ultrices justo. Suspendisse diam sapien, pretium ac tellus maximus, maximus posuere ante. Duis in augue justo.<p>
+        <p>Cras a vestibulum sem. Maecenas enim enim, porta sed tellus eget, tempor dictum metus. Praesent id odio vitae orci ultrices bibendum. Aliquam nisi diam, fermentum placerat diam sed, commodo consequat dolor. Curabitur sit amet tempus mi, id suscipit ipsum. Suspendisse facilisis ipsum quam, eu vulputate libero finibus ac. Donec quis tincidunt diam. In fringilla euismod elit, a auctor diam congue vitae. Vivamus et mauris vitae dolor condimentum pretium. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+    </div>
 
-?>
+    <div class="icon">
+        <span>£££</span>
+    </div>
+</main>
+</body>
+</html>
